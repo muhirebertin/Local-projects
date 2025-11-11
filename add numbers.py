d19e4ed5df504814651f2c773996ee7453f2ec6e
I. Muhire Bertin <118999265+muhirebertin@users.noreply.github.com>
2025-11-11 16:44:02 +0200

    today work
    this is todays work

---
+++ add numbers.py
@@ -0,0 +1,7 @@
+def additions():
+    n1 = int(input("enter First number: "))
+    n2 = int(input("enter second number number: "))
+    result = n1+n2
+    return result
+result = additions()
+print("the sum of two number is ", result)
